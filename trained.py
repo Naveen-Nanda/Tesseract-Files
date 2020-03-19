23a71f3dcdd46db36b334f37043c51527e35b9f6
Naveen-Nanda <navikamob@gmail.com>
2020-03-19 15:19:17 +0530

    Update trained.py

--- trained.py
+++ trained.py
@@ -1,3 +1,7 @@
 import os
 
+os.remove('D:/Important/Tesseract-OCR/tessdata/eng.traineddata')
+
 print(os.system('cmd /c "D:/Important/Tesseract-OCR/lstmtraining.exe --stop_training --continue_from D:/Important/_checkpoint --traineddata C:/Users/praarun1/Desktop/eng.traineddata --model_output D:/Important/Tesseract-OCR/tessdata/"'))
+
+os.rename('lstmtrain','eng.traineddata')
