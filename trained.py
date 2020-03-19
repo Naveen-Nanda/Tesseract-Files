f799e49cfa30344469b218dc92e995cb911c084e
Naveen-Nanda <navikamob@gmail.com>
2020-03-19 14:55:05 +0530

    Add files via upload

---
+++ trained.py
@@ -0,0 +1,3 @@
+import os
+
+print(os.system('cmd /c "D:/Important/Tesseract-OCR/lstmtraining.exe --stop_training --continue_from D:/Important/_checkpoint --traineddata C:/Users/praarun1/Desktop/eng.traineddata --model_output D:/Important/Tesseract-OCR/tessdata/"'))
